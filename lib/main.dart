19a2623bac1901041c440cc8aa5ad8b718773b92
Roby <robythomas97@gmail.com>
2023-10-18 20:43:02 +0100

    Display the name of the cocktail.

--- lib/main.dart
+++ lib/main.dart
@@ -26,36 +26,47 @@ class RandomCocktailPage extends StatefulWidget {
 
 class _RandomCocktailPageState extends State<RandomCocktailPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Random Cocktail'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Random Cocktail'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
             Text('Press the button to load a random cocktail!'),
-            ElevatedButton(
-              onPressed: _fetchRandomCocktail,
-              child: Text('Random Cocktail'),
+          ElevatedButton(
+            onPressed: _fetchRandomCocktail,
+            child: Text('Random Cocktail'),
+          ),
+          SizedBox(height: 20),
+          // Add a container to display the cocktail name
+          _cocktailName == null
+              ? Container()
+              : Text(
+                  _cocktailName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+          SizedBox(height: 20),
+          // Add a container to display the cocktail image
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
             ),
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-              ),
-              child: _cocktailImage == null
-                  ? Container()
-                  : Image.network(_cocktailImage),
-            ),
-          ],
-        ),
+            child: _cocktailImage == null
+                ? Container()
+                : Image.network(_cocktailImage),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
   
+  String _cocktailName = 'Cocktail Name';
   String _cocktailImage = 'https://www.thecocktaildb.com/images/logo.png';
 
   // Function to fetch a random cocktail
@@ -66,6 +77,7 @@ class _RandomCocktailPageState extends State<RandomCocktailPage> {
   var data = json.decode(response.body);
   setState(() {
     _cocktailImage = data['drinks'][0]['strDrinkThumb'];
+    _cocktailName = data['drinks'][0]['strDrink'];
   });
 }
 }
