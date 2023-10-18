87790cac1256e15805b52fe87f6cf74546bdec79
Roby <robythomas97@gmail.com>
2023-10-18 20:50:38 +0100

    Moved RandomCocktailPage to its own file.

---
+++ lib/random_cocktail_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RandomCocktailPage extends StatefulWidget {
+  @override
+  _RandomCocktailPageState createState() => _RandomCocktailPageState();
+}
+
+class _RandomCocktailPageState extends State<RandomCocktailPage> {
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Random Cocktail'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+            Text('Press the button to load a random cocktail!'),
+          ElevatedButton(
+            onPressed: _fetchRandomCocktail,
+            child: Text('Random Cocktail'),
+          ),
+          SizedBox(height: 20),
+          // Add a container to display the cocktail name
+          _cocktailName == null
+              ? Container()
+              : Text(
+                  _cocktailName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+          SizedBox(height: 20),
+          // Add a container to display the cocktail image
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+            ),
+            child: _cocktailImage == null
+                ? Container()
+                : Image.network(_cocktailImage),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+  
+  String _cocktailName = 'Cocktail Name';
+  String _cocktailImage = 'https://www.thecocktaildb.com/images/logo.png';
+
+  // Function to fetch a random cocktail
+  void _fetchRandomCocktail() async {
+  var url = Uri.parse('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+  var response = await http.get(url);
+
+  var data = json.decode(response.body);
+  setState(() {
+    _cocktailImage = data['drinks'][0]['strDrinkThumb'];
+    _cocktailName = data['drinks'][0]['strDrink'];
+  });
+}
+}
